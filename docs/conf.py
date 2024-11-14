bb09e224c40b6338a4b595c0e3a8826a2adf83e3
Nirav Lab <51907189+NiravBhattLab@users.noreply.github.com>
2024-11-14 19:32:01 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_favicon = 'favicon.jpg'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
