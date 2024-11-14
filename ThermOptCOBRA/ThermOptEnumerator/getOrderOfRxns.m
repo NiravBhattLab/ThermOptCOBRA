78f92d77dbcca11eafc006685048289f64680763
Nirav Lab <51907189+NiravBhattLab@users.noreply.github.com>
2024-11-14 18:04:31 +0530

    Update getOrderOfRxns.m

--- ThermOptCOBRA/ThermOptEnumerator/getOrderOfRxns.m
+++ ThermOptCOBRA/ThermOptEnumerator/getOrderOfRxns.m
@@ -1,4 +1,22 @@
 function [order,bins,binsizes] =getOrderOfRxns(model)
+% To get the order of reactions based on the connected components obtained
+%
+% USAGE: 
+%   [order,bins,binsizes] =getOrderOfRxns(model)
+%
+% INPUTS:
+%     model:     COBRA model structure for which the order of reaction has to be obtained
+%
+% OUTPUTS:
+%     order:      Reaction IDs
+%     bins:       Bins defining the IDs of connected components in which the reactions belong
+%     binsizes:   Size of the connected components obtained
+%
+% .. Author:
+%       - Pavan Kumar S, BioSystems Engineering and control (BiSECt) lab, IIT Madras
+
+
+
 S = double(model.S~=0); S=logical(S'*S);
 g = graph(S);
 try
